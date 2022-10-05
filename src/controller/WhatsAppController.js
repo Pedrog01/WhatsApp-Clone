109db38524a867bec4c35e42bdc597427d7c39e3
Pedro <pedro7109.j@gmail.com>
2022-10-05 11:34:26 -0300

    prototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,12 +3,14 @@ class WhatsAppController {
     constructor(){
 
     this.loadElements();
+    this.elementsPrototype();
 
 
     }
 
 
     
+
     loadElements(){
 
         this.el = {};
@@ -18,6 +20,75 @@ class WhatsAppController {
             this.el[Forma.getCamelCase(element.id)] = element;
 
         });
+
+    }
+
+
+    elementsPrototype(){
+
+        Element.prototype.hide = function(){
+
+                Element.prototype.hide = function(){
+        
+                    this.style.display = 'none';
+                    return this;
+                }
+        
+                Element.prototype.show = function(){
+        
+                    this.style.display = 'block';
+                    return this;
+                }
+        
+        
+                Element.prototype.toggle = function(){
+        
+                    this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+                    return this;
+        
+                }
+        
+                Element.prototype.on = function(events, fn){
+                    events.spli('').forEach(event =>{
+                        this.addEventListener(event, fn);
+        
+                    });
+                    return this;
+                
+                }
+        
+                Element.prototype.css = function(styles){
+                  for (let name in styles) {
+                        this.style[name] = styles[name];
+                  }
+                  return this;
+                
+                }
+        
+                Element.prototype.addClass = function(name){
+                    this.classList.add(name);
+                    return this;
+                  
+                  }
+        
+                  Element.prototype.removeClass = function(name){
+                    this.classList.remove(name);
+                    return this;
+                  
+                  }
+        
+                  Element.prototype.toggleClass = function(name){
+                    this.classList.toggle(name);
+                    return this;
+                  
+                  }
+        
+                  Element.prototype.hasClass = function(name){
+                    return this.classList.contains(name);
+                  
+                  }
+
+        }
 
     }
 
