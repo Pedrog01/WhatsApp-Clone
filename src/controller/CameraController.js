2ab614dbcfe48985053e0de5f61fcff883abbcc5
Pedro <pedro7109.j@gmail.com>
2022-10-07 10:50:56 -0300

    Stopping capturing camera image

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -8,10 +8,20 @@
             video: true
         }).then(stream =>{
 
+            
+            this._stream = stream;
             this._videoEl.src = URL.createObjectURL(stream);
             this._videoEl.play();
         }).catch(err=>{
             console.error(err)
         });
     }
+
+stop(){
+  
+    this._stream.getTracks().forEach(track =>{
+        track.stop();
+    });
+  }
+
 }
