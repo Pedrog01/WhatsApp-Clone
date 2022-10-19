60a0e870272462b5cd372af13559b577eef55d5b
Pedro <pedro7109.j@gmail.com>
2022-10-19 09:34:17 -0300

    Send message to attached contact

--- src/model/message.js
+++ src/model/message.js
@@ -97,11 +97,6 @@ export class Message extends Model{
                     img.show();
                 }
 
-                div.querySelector('.btn-message-send').on('click', e=>{
-
-                    console.log('Enviar mensagem')
-
-                });
 
             break;
 
